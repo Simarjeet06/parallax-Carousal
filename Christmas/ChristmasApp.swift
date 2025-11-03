4e9bf381992b8f84f3566c2917cc6cec0326bcce
Simarjeet Kaur <simarjeetkaur6603@gmail.com>
2025-11-03 19:59:10 +0530

    Initial Commit

---
+++ Christmas/ChristmasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChristmasApp.swift
+//  Christmas
+//
+//  Created by Simarjeet Kaur on 03/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ChristmasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
